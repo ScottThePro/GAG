0621a2740c059903a4e3b798909ced649d136908
ScottThePro <82802294+ScottThePro@users.noreply.github.com>
2025-11-16 23:05:55 +1300

    Update script.lua

--- script.lua
+++ script.lua
@@ -61,7 +61,7 @@ local Window = Rayfield:CreateWindow({
    LoadingSubtitle = "by noone",
    Theme = "Default", -- Check https://docs.sirius.menu/rayfield/configuration/themes
 
-   DisableRayfieldPrompts = false,
+   DisableRayfieldPrompts = true,
    DisableBuildWarnings = false, -- Prevents Rayfield from warning when the script has a version mismatch with the interface
 
    ConfigurationSaving = {
